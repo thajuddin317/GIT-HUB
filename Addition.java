67d258f8bb22b58c8372d94e2d34dd8c0a2c57d3
thajuddin317 <thajuddin317@gmail.com>
2022-02-02 18:45:51 +0530

    add my first file to repo

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class addition 
+{
+	public static void main (strin args [])
+	{
+		int num1,num2,res;
+		system.output.println("enter first number value");
+		scanner s = new snnar(system.in);
+		num1=s.nextint( );
+		system.out.println("enter second number value");
+		num2=s.nextint( );
+		res=num1+num2;
+		system.out.println("result is "+res);
+	}
+}
